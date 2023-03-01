e66a06c7e666c1798ba604157df6e0ba74b9cb77
johnny-mh <romz1212@gmail.com>
2023-03-01 17:58:27 +0900

    refactor(astro-blog): 포스트 상세까지 완료

--- apps/astro-blog/astro.config.mjs
+++ apps/astro-blog/astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
+
 import { remarkReadingTime } from './remarks/readingTime.mjs'
 import { remarkCreatedAt } from './remarks/createdAt.mjs'
+import { rehypePrettyCode } from './remarks/rehypePrettyCode.mjs'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,8 +17,9 @@ export default defineConfig({
     preact(),
   ],
   markdown: {
-    remarkPlugins: [remarkReadingTime, remarkCreatedAt],
+    syntaxHighlight: false,
     extendDefaultPlugins: true,
-    syntaxHighlight: 'prism',
+    remarkPlugins: [remarkReadingTime, remarkCreatedAt],
+    rehypePlugins: [rehypePrettyCode],
   },
 })
