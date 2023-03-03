cc5ea72c6fd4d3c9b69b43d2874837312b386e93
nz10382 <nz10382@naverz-corp.com>
2023-03-03 10:06:15 +0900

    astro 블로그 정리

--- apps/astro-blog/remarks/createdAt.mjs
+++ apps/astro-blog/remarks/createdAt.mjs
@@ -7,14 +7,13 @@ export function remarkCreatedAt() {
   const POST_FILENAME_REGEX = /(\d{4})-(\d{2})-(\d{2})-(.+)/
 
   return function (_, { data, history }) {
+    if (data.astro.frontmatter.title === 'about') {
+      return
+    }
+
     const [fileName] = history
     const dirName = fileName.split('/').at(-1)
-    const match = POST_FILENAME_REGEX.exec(dirName)
-
-    if (!match) {
-      throw new Error(`컨텐츠 경로 규칙 확인 요망: ${fileName}`)
-    }
-    const [, year, month, day] = Array.from(match)
+    const [, year, month, day] = POST_FILENAME_REGEX.exec(dirName) ?? []
 
     data.astro.frontmatter.createdAt = format(
       new Date(Number(year), Number(month) - 1, Number(day)),
