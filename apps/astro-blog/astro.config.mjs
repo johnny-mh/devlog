baf1a7757b91fc951d33695deb36900234bc77a6
johnny-mh <romz1212@gmail.com>
2023-03-03 11:57:34 +0900

    adding sitemap, robots

--- apps/astro-blog/astro.config.mjs
+++ apps/astro-blog/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
 
 import { remarkReadingTime } from './remarks/readingTime.mjs'
 import { remarkCreatedAt } from './remarks/createdAt.mjs'
@@ -9,12 +11,13 @@ import { rehypePrettyCode } from './remarks/rehypePrettyCode.mjs'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://johnny-mh.github.io',
   integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
     mdx(),
     preact(),
+    sitemap(),
+    robotsTxt(),
   ],
   markdown: {
     syntaxHighlight: false,
